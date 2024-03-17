92b08782e93fae8d80d75491bdc43c0c71cfb63b
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2024-03-17 22:30:49 +0500

    Step 20 of Typescript
    Step 20 of Typescript

---
+++ Q20.js
@@ -0,0 +1,17 @@
+"use strict";
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+// Example 01
+console.log("\nExample1", "-".repeat(10));
+let language = [
+    "English", "Urdu", "Arabic", "British"
+];
+language.map((item) => console.log(item));
+// Example 02
+console.log("\nExample1", "-".repeat(10));
+// List of mountains
+const mountains = ["K2", "Everst", "Kangchenjunga", "Lhotse", "Malaku"];
+//  print the list of mountains
+console.log("List of Mountains:");
+for (const mountain of mountains) {
+    console.log(mountains);
+}
---
+++ Q20.ts
@@ -0,0 +1,21 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+// Example 01
+console.log("\nExample1","-".repeat(10))
+
+let language: string[] = [
+    "English","Urdu", "Arabic", "British"];
+    language.map((item) => 
+    console.log(item));
+
+// Example 02
+console.log("\nExample1","-".repeat(10))
+
+// List of mountains
+const mountains: string[] = ["K2", "Everst", "Kangchenjunga", "Lhotse", "Malaku"];
+
+//  print the list of mountains
+console.log("List of Mountains:");
+for (const mountain of mountains) {
+    console.log(mountains)
+}
